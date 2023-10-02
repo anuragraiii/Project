b19ab0b5ed0f066651cbdc9b088ca86401d199e6
Anurag Rai <90954348+anuragraiii@users.noreply.github.com>
2023-10-02 08:27:17 +0530

    Add files via upload

---
+++ Nike Landing Page/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navigation from "./Components/Navigation";
+import Herosection from "./Components/HeroSection";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Herosection />
+    </div>
+  );
+};
+export default App;
---
+++ Nike Landing Page/src/Components/HeroSection.jsx
@@ -0,0 +1,33 @@
+const Herosection = () => {
+  return (
+    <section className="hero">
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-lg-7">
+            <h1>
+              YOUR FEET DESERVE <br />
+              THE BEST
+            </h1>
+            <br />
+            <p>
+              YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+              SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH
+              OUR SHOES.
+            </p>
+            <p>Also Available On</p>
+            <img src="/ProjectImages/amazon.png" alt="amazonpng" />
+            <img src="/ProjectImages/flipkart.png" alt="flipkart.png" />
+          </div>
+          <div className="col-lg-5 img-hero">
+            <img
+              src="/ProjectImages/shoe_image.png"
+              alt="shoehero"
+              className="img-hero"
+            ></img>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Herosection;
